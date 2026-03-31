84eda9c67438457180b541e4ab176dae0c5ac7a7
saradevlop7 <elkbircharafi8@gmail.com>
2026-03-31 16:49:10 +0200

    feat: display games dynamically from dataset

--- script.js
+++ script.js
@@ -4,13 +4,19 @@
 
     container.innerHTML = '';
     gamesToShow.forEach(game => {
-        const item = document.createElement('div');
-        item.className = 'col-12 mb-3';
-        item.innerHTML = `
-            <h5>${game.title}</h5>
-            <p>${game.price}€</p>
+        const card = document.createElement('div');
+        card.className = 'col-12 mb-4';
+        card.innerHTML = `
+            <div class="card h-100">
+                <img src="${game.image}" class="card-img-top" alt="${game.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${game.title}</h5>
+                    <p class="card-text fw-bold">Prix: ${game.price}€</p>
+                    <button class="btn btn-primary add-to-cart" data-id="${game.id}">Ajouter au panier</button>
+                </div>
+            </div>
         `;
-        container.appendChild(item);
+        container.appendChild(card);
     });
 }
 
