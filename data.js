a19803146149af966abad8c31795da381db217e5
saradevlop7 <elkbircharafi8@gmail.com>
2026-03-31 16:48:58 +0200

    chore: add initial game dataset

---
+++ data.js
@@ -0,0 +1,24 @@
+﻿// data.js - Données des jeux vidéo
+const games = [
+    {
+        id: 1,
+        title: "The Legend of Zelda: Breath of the Wild",
+        image: "https://upload.wikimedia.org/wikipedia/en/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg",
+        price: 59.99,
+        genre: "Action"
+    },
+    {
+        id: 2,
+        title: "The Witcher 3: Wild Hunt",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/0c/Witcher_3_cover_art.jpg",
+        price: 39.99,
+        genre: "RPG"
+    },
+    {
+        id: 3,
+        title: "Call of Duty: Modern Warfare",
+        image: "https://upload.wikimedia.org/wikipedia/en/5/52/Call_of_Duty_Modern_Warfare_%282019%29_cover.jpg",
+        price: 49.99,
+        genre: "FPS"
+    }
+];
